fd61d82a3bafe26dd56fa10c8f294525db597a3c
Mason Skellton <masonskellton@macbook-air.mynetworksettings.com>
2024-04-30 09:31:51 -0500

    save and cancel  buttons not hiding

--- client/resources/scripts/index.js
+++ client/resources/scripts/index.js
@@ -3,7 +3,7 @@ function handleOnLoad(){
 }
 
 function handleOnChange(){
-    const selectedId = getElementById("selectListBox").value; 
+    const selectedId = document.getElementById("selectListBox").value;
     bookList.forEach((book)=>{
         if(book.id == selectedId){
             myBook = book; 
@@ -16,8 +16,8 @@ function handleOnChange(){
 function handleEditClick(){
     makeEditable(); 
     hideButtons(); 
-    var bettonHtml = "<button class=\"btn btn-primary btn-lg\"onclick=\"handeEditSave("+myBook.id+")\">Save<\button>"
-    buttonHtml += "<button class=\"btn btn-warning btn-cancle\" onclick=\"handleCancelSave()\"<Cancel<\button>"
+    var bettonHtml = "<button class=\"btn btn-primary btn-lg\"onclick=\"handeEditSave("+myBook.id+")\">Save</button>"
+    buttonHtml += "<button class=\"btn btn-warning btn-cancle\" onclick=\"handleCancelSave()\">Cancel</button>"
     document.getElementById("saveButton").innerHTML = buttonHtml; 
     document.getElementById("saveButton").style.display = "inline-block";
 }
@@ -26,8 +26,8 @@ function handleNewClick(){
     makeEditable(); 
     hideButtons(); 
     blankFields(); 
-    var buttonHtml = "<button class=\"btn btn-primary btn-lg\" onclick=\"handleNewSave()\">Save<\button>"
-    buttonHtml += "<button class=\"btn btn-warning btn-lg btn-cancle\" onclick=\"handleCancelSave()\">Cancel<\button>"
+    var buttonHtml = "<button class=\"btn btn-primary btn-lg\" onclick=\"handleNewSave()\">Save</button>"
+    buttonHtml += "<button class=\"btn btn-warning btn-lg btn-cancle\" onclick=\"handleCancelSave()\">Cancel</button>"
     document.getElementById("saveButton").innerHTML = buttonHtml; 
     document.getElementById("saveButton").style.display = "inline-block";
 }
@@ -72,7 +72,7 @@ function populateForm(){
     document.getElementById("bookAuthor").value = myBook.author; 
     document.getElementById("bookGenre").value = myBook.genre; 
     document.getElementById("bookAvlb").value = myBook.numAvlb; 
-    document.getElementById("bookIbsn").value = myBook.ibsn; 
+    document.getElementById("bookIsbn").value = myBook.isbn; 
     document.getElementById("bookLength").value = myBook.length; 
     document.getElementById("bookCover").value = myBook.cover;
     var html = "<img class = \"coverArt\" src= \"" + myBook.cover + "\"></img>";
@@ -100,7 +100,7 @@ function hideButtons(){
     document.getElementById("bookAuthor").readOnly=false; 
     document.getElementById("bookGenre").readOnly=false; 
     document.getElementById("bookAvlb").readOnly=false; 
-    document.getElementById("bookIbsn").readOnly=false; 
+    document.getElementById("bookIsbn").readOnly=false; 
     document.getElementById("bookLength").readOnly=false; 
     document.getElementById("bookCover").readOnly=false;
  }
@@ -110,7 +110,7 @@ function hideButtons(){
     document.getElementById("bookAuthor").value="";  
     document.getElementById("bookGenre").value="";  
     document.getElementById("bookAvlb").value="";  
-    document.getElementById("bookIbsn").value="";  
+    document.getElementById("bookIsbn").value="";  
     document.getElementById("bookLength").value="";  
     document.getElementById("bookCover").value=""; 
  }
@@ -120,7 +120,7 @@ function hideButtons(){
     document.getElementById("bookAuthor").readOnly=true;  
     document.getElementById("bookGenre").readOnly=true;  
     document.getElementById("bookAvlb").readOnly=true;  
-    document.getElementById("bookIbsn").readOnly=true;  
+    document.getElementById("bookIsbn").readOnly=true;  
     document.getElementById("bookLength").readOnly=true;  
     document.getElementById("bookCover").readOnly=true;
  }
